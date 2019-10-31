ebf75ed4aad57b213a6be92d4d6582994281dd89
Mateusz <mrpeciak@gmail.com>
2019-10-31 08:36:28 +0100

    minor tweaks in index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import throttle from 'lodash.throttle';
 
 import './index.scss';
 import App from './App/App';
 import reducer from './store/reducer';
 import * as serviceWorker from './serviceWorker';
-// import { loadState, saveState } from './store/localStorage'
 
 let store = createStore(reducer);
 

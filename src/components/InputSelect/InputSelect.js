0a9d2808746ecdb5063d7e56b9fa5b1c5c11c8f7
Mateusz <mrpeciak@gmail.com>
2019-10-31 08:40:30 +0100

    add and style main input

---
+++ src/components/InputSelect/InputSelect.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './InputSelect.scss';
+
+const InputSelect = props => {
+  return (
+    <input
+      type="text"
+      className="input-select"
+    />
+  );
+};
+
+
+export default InputSelect

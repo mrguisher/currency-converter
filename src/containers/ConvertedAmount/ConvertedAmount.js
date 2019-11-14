50869d8b1c9c7dd66a7b78eaa32bd9f1f66b82ba
Mateusz <mrpeciak@gmail.com>
2019-11-14 22:29:09 +0100

    add redux state and dispatch to convertedAmount component

--- src/containers/ConvertedAmount/ConvertedAmount.js
+++ src/containers/ConvertedAmount/ConvertedAmount.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import './ConvertedAmount.scss';
 
@@ -7,22 +8,33 @@ const ConvertedAmount = props => {
     <div className="converted-amount">
       <p className="converted-amount__results">
         <span className="converted-amount__results--value">
-
+          {props.amountConvertedFrom}
         </span>
         <span className="converted-amount__results--currency">
-
+          {props.currencyConvertedFrom}
         </span>
         <span className="converted-amount__results__equal">=</span>
         <span className="converted-amount__results--value">
-
+          {props.amountConvertedTo}
         </span>
         <span className="converted-amount__results--currency">
-
+          {props.currencyConvertedTo}
         </span>
       </p>
     </div>
   );
 };
 
+const mapStateToProps = state => {
+  return {
+    amountConvertedFrom: state.amountConvertedFrom,
+    amountConvertedTo: state.amountConvertedTo,
+    currencyConvertedTo: state.currencyConvertedTo,
+    currencyConvertedFrom: state.currencyConvertedFrom,
+  };
+};
 
-export default ConvertedAmount 
+export default connect(
+  mapStateToProps,
+  null
+)(ConvertedAmount);
